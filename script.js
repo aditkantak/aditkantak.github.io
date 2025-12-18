6e42cfc3dd6fa420072deba5d471887519e2aad4
Adit Kantak <aditpk8@gmail.com>
2025-12-18 12:48:33 -0800

    Made theme persistent across reloads

---
+++ script.js
@@ -0,0 +1,20 @@
+document.addEventListener("DOMContentLoaded", (event) => {
+    const theme = window.localStorage.getItem("theme");
+    if (theme == null){
+        window.localStorage.setItem("theme", "cafe");
+    }
+    else if (theme == "olivia"){
+        document.body.classList.add("olivia");
+    }
+});
+
+document.getElementById("theme-switcher").addEventListener("click", () => {
+    let body = document.body;
+    body.classList.toggle("olivia");
+    if (body.classList.contains("olivia")){
+        window.localStorage.setItem("theme", "olivia");
+    }
+    else {
+        window.localStorage.setItem("theme", "cafe");
+    }
+});
